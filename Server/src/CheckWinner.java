f97f6ea539279038414429587ea54f8d9346d1b3
redf1sh <unreal.770@gmail.com>
2018-05-24 03:24:16 +0300

    Добавлены шаблоны для обработки победы

---
+++ Server/src/CheckWinner.java
@@ -0,0 +1,47 @@
+public class CheckWinner {
+    /*
+    public String[] checkWin(){
+        //Создаём массив победителей. Раздача кэша будет Bank/length winners
+        String winners[];
+
+        return winners;
+    }
+*/
+    //TODO
+    //Передаём параметром 2 карты игрока и 5 карт на столе. Если победитель, то записываем его в winners
+    private boolean isRoyalFlush(CardDeck.Card[] userCards, CardDeck.Card[] tableCards){
+        return true;
+    }
+
+    private boolean isStraightFlush(CardDeck.Card[] userCards, CardDeck.Card[] tableCards){
+        return true;
+    }
+
+    private boolean isFourOfaKind(CardDeck.Card[] userCards, CardDeck.Card[] tableCards){
+        return true;
+    }
+
+    private boolean isFullHouse(CardDeck.Card[] userCards, CardDeck.Card[] tableCards){
+        return true;
+    }
+
+    private boolean isStraight(CardDeck.Card[] userCards, CardDeck.Card[] tableCards){
+        return true;
+    }
+
+    private boolean isThreeOfaKind(CardDeck.Card[] userCards, CardDeck.Card[] tableCards){
+        return true;
+    }
+
+    private boolean isTwoPairs(CardDeck.Card[] userCards, CardDeck.Card[] tableCards){
+        return true;
+    }
+
+    private boolean isPair(CardDeck.Card[] userCards, CardDeck.Card[] tableCards){
+        return true;
+    }
+
+    private boolean isHighCard(CardDeck.Card[] userCards, CardDeck.Card[] tableCards){
+        return true;
+    }
+}
